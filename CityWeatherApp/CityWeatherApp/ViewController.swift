ddae6ecd27a6641d0167eed3ea0d663e4754e558
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-03 09:53:00 +0300

    Adding separate project `CityWeatherApp` as entry point of application and modular approach.

---
+++ CityWeatherApp/CityWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CityWeatherApp
+//
+//  Created by Georgi Teoharov on 3.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
