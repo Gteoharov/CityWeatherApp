8cb97b56fefbb366e23b64f6dae8dfb5474a75c2
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-03 16:46:03 +0300

    Forgot to add buildRequest helper method in private extension that I create earlier

--- CityWeatherCore/CityWeatherCore/CityAPIClient/URLSessionClient.swift
+++ CityWeatherCore/CityWeatherCore/CityAPIClient/URLSessionClient.swift
@@ -14,7 +14,7 @@ public final class URLSessionClient: HTTPClient {
     
     public func perform(request: URLRequest, queryItems: [URLQueryItem]?) async -> HTTPClient.HTTPClientResult {
         do {
-            let (data, response) = try await session.data(for: request)
+            let (data, response) = try await session.data(for: buildRequest(from: request, with: queryItems))
             if let response = response as? HTTPURLResponse {
                 return .success((data, response))
             }
