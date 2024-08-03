7d2fbe58ceb6d00b5f9e87c695270f3caed85b16
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-03 13:08:55 +0300

    Adding helper folder to test target and creating XCTestCase extension that will be used in every makeSUT( system under test) for memory leaks)

---
+++ CityWeatherCore/CityWeatherCoreTests/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,12 @@
+import XCTest
+
+// Track memory leak extension
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject,
+                                     file: StaticString = #file,
+                                     line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been dellocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
