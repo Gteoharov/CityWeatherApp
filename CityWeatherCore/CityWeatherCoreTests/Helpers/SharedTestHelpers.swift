746347de8c2afbc8c063ae1a9b8ec791908b4d5a
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-03 13:22:52 +0300

    Adding SharedTestHelpers that will be used a cross test modules for readability and clean code

---
+++ CityWeatherCore/CityWeatherCoreTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+func anyURL() -> URL {
+    URL(string: "http://any-url.com")!
+}
+
+func anyData() -> Data {
+    Data("any data".utf8)
+}
+
+func makeURLRequest() -> URLRequest {
+    URLRequest(url: anyURL())
+}
+
+func nonHTTPURLResponse() -> URLResponse {
+    URLResponse(url: anyURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+}
+
+func anyHTTPURLResponse() -> HTTPURLResponse {
+    HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)!
+}
+
+func buildURL(with query: String, url: URL) -> URL {
+    url.appending(queryItems: [URLQueryItem(name: "kv", value: query)])
+}
+
+func createQuery() -> String {
+    "Paris"
+}
