93d10282ded08115ee73a56709e805afdf13c71b
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-03 09:49:28 +0300

    Create initial `CityWeatherCore` as macOS framework to run tests faster, to hold the core logic and approach with modular architecture.

---
+++ CityWeatherCore/CityWeatherCore/CityWeatherCore.h
@@ -0,0 +1,18 @@
+//
+//  CityWeatherCore.h
+//  CityWeatherCore
+//
+//  Created by Georgi Teoharov on 3.08.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CityWeatherCore.
+FOUNDATION_EXPORT double CityWeatherCoreVersionNumber;
+
+//! Project version string for CityWeatherCore.
+FOUNDATION_EXPORT const unsigned char CityWeatherCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CityWeatherCore/PublicHeader.h>
+
+
