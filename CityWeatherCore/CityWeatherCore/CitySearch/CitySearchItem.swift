542d401463d8048b172f5cec355b700c4b76e6f7
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-03 15:56:38 +0300

    Fixing typo in `CitySearchItem` domain model

--- CityWeatherCore/CityWeatherCore/CitySearch/CitySearchItem.swift
+++ CityWeatherCore/CityWeatherCore/CitySearch/CitySearchItem.swift
@@ -3,14 +3,14 @@ import Foundation
 public struct CitySearchItem: Equatable {
     public let name: String
     public let latitude: Double
-    public let longitutde: Double
+    public let longitude: Double
     public let country: String
     public let state: String?
     
-    public init(name: String, latitude: Double, longitutde: Double, country: String, state: String?) {
+    public init(name: String, latitude: Double, longitude: Double, country: String, state: String?) {
         self.name = name
         self.latitude = latitude
-        self.longitutde = longitutde
+        self.longitude = longitude
         self.country = country
         self.state = state
     }
