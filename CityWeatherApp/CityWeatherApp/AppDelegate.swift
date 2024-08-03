119683ce090882daff8070e8a2864d13153f7f8a
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-03 09:54:18 +0300

    Removing not needed comments

--- CityWeatherApp/CityWeatherApp/AppDelegate.swift
+++ CityWeatherApp/CityWeatherApp/AppDelegate.swift
@@ -1,16 +1,7 @@
-//
-//  AppDelegate.swift
-//  CityWeatherApp
-//
-//  Created by Georgi Teoharov on 3.08.24.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
--- CityWeatherApp/CityWeatherApp/ViewController.swift
+++ CityWeatherApp/CityWeatherApp/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  CityWeatherApp
-//
-//  Created by Georgi Teoharov on 3.08.24.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,7 +6,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
