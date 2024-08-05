09cc17f6e86a529ffd2b766d9c8d6097fc1e855c
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-05 21:42:05 +0300

    Setting up iOS framework for target 15

---
+++ CityWeatherCore/CityWeatheriOS/Screens/DetailWeatherCityScreen/DetailWeatherCityScreen.swift
@@ -0,0 +1,18 @@
+//
+//  DetailWeatherCityScreen.swift
+//  CityWeatheriOS
+//
+//  Created by Georgi Teoharov on 5.08.24.
+//
+
+import SwiftUI
+
+struct DetailWeatherCityScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailWeatherCityScreen()
+}
