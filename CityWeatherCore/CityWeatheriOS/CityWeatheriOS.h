730055801d467c2b54594380a550ae5546e14667
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-05 11:32:04 +0300

    Setting up iOS framework inside core for UI

---
+++ CityWeatherCore/CityWeatheriOS/CityWeatheriOS.h
@@ -0,0 +1,18 @@
+//
+//  CityWeatheriOS.h
+//  CityWeatheriOS
+//
+//  Created by Georgi Teoharov on 5.08.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CityWeatheriOS.
+FOUNDATION_EXPORT double CityWeatheriOSVersionNumber;
+
+//! Project version string for CityWeatheriOS.
+FOUNDATION_EXPORT const unsigned char CityWeatheriOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CityWeatheriOS/PublicHeader.h>
+
+
