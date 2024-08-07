9db1899b6d6ed619a4427f9878d184b74c659484
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-07 12:55:10 +0300

    Polishing Detail weather screen (wip)

--- CityWeatherCore/CityWeatheriOS/Screens/DetailWeatherCityScreen/DetailWeatherCityScreen.swift
+++ CityWeatherCore/CityWeatheriOS/Screens/DetailWeatherCityScreen/DetailWeatherCityScreen.swift
@@ -1,5 +1,7 @@
 import SwiftUI
 import Combine
+import CityWeatherCore
+
 struct DetailWeatherCityScreen: View {
     @ObservedObject var viewModel: CityDetailViewModel
     
@@ -8,92 +10,60 @@ struct DetailWeatherCityScreen: View {
             VStack {
                 if viewModel.isLoading {
                     ProgressView()
-                } else if let weatherResponse = viewModel.weatherResponse {
-                    WeatherInfoView(weatherResponse: weatherResponse, weatherIcon: viewModel.weatherIcon)
+                } else if let weatherResponse = viewModel.city {
+                    WeatherInfoView(weatherResponse: weatherResponse)
                         .transition(.opacity)
-                        .animation(.easeInOut(duration: 0.5), value: 10)
+                        .animation(.easeInOut(duration: 1), value: 10)
                 } else {
                     Text("No weather data available")
                         .foregroundColor(.gray)
                 }
             }
-            .onAppear {
-                viewModel.fetchWeather()
+            .task {
+                viewModel.fetchCityDetail()
             }
-            .refreshable {
-                viewModel.fetchWeather()
+            
+        }
+        .refreshable {
+            viewModel.fetchCityDetail()
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    // Button action
+                }) {
+                    Image(systemName: "thermometer.high")
+                }
             }
         }
     }
 }
 
 struct WeatherInfoView: View {
-    let weatherResponse: WeatherResponse
-    let weatherIcon: UIImage?
+    let weatherResponse: CityDetailItem
     
     var body: some View {
         VStack {
-            Text("\(Int(weatherResponse.main.temp))°")
+            Text("\(Int(weatherResponse.mainWeather.temp))°")
                 .font(.largeTitle)
                 .bold()
             Text(weatherResponse.weather.first?.description ?? "")
                 .font(.title2)
                 .foregroundColor(.gray)
-            if let icon = weatherIcon {
-                Image(uiImage: icon)
-                    .resizable()
-                    .frame(width: 100, height: 100)
+            if let icon = weatherResponse.weather.first?.icon {
+                let iconURL = URL(string: "https://openweathermap.org/img/wn/\(icon)@2x.png")!
+                AsyncImage(url: iconURL) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                } placeholder: {
+                    ProgressView()
+                }
             }
             Text(weatherResponse.name)
                 .font(.title)
         }
-    }
-}
-
-
-
-
-
-
-
-struct WeatherResponse: Codable {
-    let coord: Coord
-    let weather: [Weather]
-    let main: Main
-    let wind: Wind
-    let sys: Sys
-    let name: String
-    
-    struct Coord: Codable {
-        let lon: Double
-        let lat: Double
-    }
-    
-    struct Weather: Codable, Identifiable {
-        let id: Int
-        let main: String
-        let description: String
-        let icon: String
-    }
-    
-    struct Main: Codable {
-        let temp: Double
-        let feels_like: Double
-        let temp_min: Double
-        let temp_max: Double
-        let pressure: Int
-        let humidity: Int
-    }
-    
-    struct Wind: Codable {
-        let speed: Double
-        let deg: Int
-    }
-    
-    struct Sys: Codable {
-        let country: String
-        let sunrise: Int
-        let sunset: Int
     }
 }
 
