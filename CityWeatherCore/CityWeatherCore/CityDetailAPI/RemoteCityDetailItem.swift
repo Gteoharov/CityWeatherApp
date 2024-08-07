a765227fdcaf4a99e85ba020ff6b73da025820a6
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-07 09:53:43 +0300

    Creating `RemoteCityDetailItem` dto model

--- CityWeatherCore/CityWeatherCore/CityDetailAPI/RemoteCityDetailItem.swift
+++ CityWeatherCore/CityWeatherCore/CityDetailAPI/RemoteCityDetailItem.swift
@@ -1,8 +1,56 @@
-//
-//  RemoteCityDetailItem.swift
-//  CityWeatherCore
-//
-//  Created by Georgi Teoharov on 7.08.24.
-//
-
 import Foundation
+
+struct RemoteCityDetailItem: Decodable {
+    let cord: Coord
+    let weather: [Weather]
+    let base: String
+    let main: [Main]
+    let visibility: Int
+    let wind: Wind
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone: Int
+    let id: Int
+    let name: String
+    let cod: Int
+}
+
+struct Coord: Decodable {
+    let lat: Double
+    let lon: Double
+}
+
+struct Weather: Decodable {
+    let id: Int
+    let main: String
+    let description: String
+    let icon: String
+}
+
+struct Main: Decodable {
+    let temp: Double
+    let feels_like: Double
+    let temp_min: Double
+    let temp_max: Double
+    let pressure: Int
+    let humidity: Int
+    let sea_level: Int
+    let grnd_level: Int
+}
+
+struct Clouds: Decodable {
+    let all: Int
+}
+
+struct Wind: Decodable {
+    let speed: Double
+    let deg: Int
+    let gust: Double
+}
+
+struct Sys: Decodable {
+    let country: String
+    let sunrise: Int
+    let sunset: Int
+}
