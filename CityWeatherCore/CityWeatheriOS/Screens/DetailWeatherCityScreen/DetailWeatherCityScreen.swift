591560e94c49aca9310ce8027c5dabe1ab393846
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-07 09:55:47 +0300

    Moving out the logic to modules they belong

--- CityWeatherCore/CityWeatheriOS/Screens/DetailWeatherCityScreen/DetailWeatherCityScreen.swift
+++ CityWeatherCore/CityWeatheriOS/Screens/DetailWeatherCityScreen/DetailWeatherCityScreen.swift
@@ -1,9 +1,7 @@
 import SwiftUI
 import Combine
-
-
 struct DetailWeatherCityScreen: View {
-    @ObservedObject var viewModel: WeatherViewModel
+    @ObservedObject var viewModel: CityDetailViewModel
     
     var body: some View {
         ScrollView {
@@ -35,10 +33,10 @@ struct WeatherInfoView: View {
     
     var body: some View {
         VStack {
-            Text("\(Int(weatherResponse.main?.temp ?? 0 - 273.15))°")
+            Text("\(Int(weatherResponse.main.temp))°")
                 .font(.largeTitle)
                 .bold()
-            Text(weatherResponse.weather?.first?.description ?? "")
+            Text(weatherResponse.weather.first?.description ?? "")
                 .font(.title2)
                 .foregroundColor(.gray)
             if let icon = weatherIcon {
@@ -46,7 +44,7 @@ struct WeatherInfoView: View {
                     .resizable()
                     .frame(width: 100, height: 100)
             }
-            Text(weatherResponse.name ?? "")
+            Text(weatherResponse.name)
                 .font(.title)
         }
     }
@@ -54,82 +52,17 @@ struct WeatherInfoView: View {
 
 
 
-class WeatherViewModel: ObservableObject {
-    @Published var weatherResponse: WeatherResponse?
-    @Published var isLoading = false
-    @Published var weatherIcon: UIImage?
-    
-    private let city: String
-    
-    init(city: String) {
-        self.city = city
-    }
-    
-    private var cancellables = Set<AnyCancellable>()
-    private let cache = NSCache<NSString, UIImage>()
-    
-    func fetchWeather() {
-        isLoading = true
-        
-        let apiKey = "86879ac16cba5e431ae293fa564b6bf3"
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)"
-        
-        guard let url = URL(string: urlString) else {
-            isLoading = false
-            return
-        }
-        
-        URLSession.shared.dataTaskPublisher(for: url)
-            .map { $0.data }
-            .decode(type: WeatherResponse.self, decoder: JSONDecoder())
-            .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: { [weak self] completion in
-                self?.isLoading = false
-                switch completion {
-                case .failure(let error):
-                    print("Error fetching weather: \(error)")
-                case .finished:
-                    break
-                }
-            }, receiveValue: { [weak self] weatherResponse in
-                self?.weatherResponse = weatherResponse
-                if let icon = weatherResponse.weather?.first?.icon {
-                    self?.fetchWeatherIcon(icon: icon)
-                }
-            })
-            .store(in: &cancellables)
-    }
-    
-    private func fetchWeatherIcon(icon: String) {
-        let iconURL = "https://openweathermap.org/img/wn/\(icon)@2x.png"
-        
-        if let cachedImage = cache.object(forKey: iconURL as NSString) {
-            self.weatherIcon = cachedImage
-            return
-        }
-        
-        guard let url = URL(string: iconURL) else { return }
-        
-        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
-            guard let data = data, error == nil, let image = UIImage(data: data) else { return }
-            
-            DispatchQueue.main.async {
-                self?.cache.setObject(image, forKey: iconURL as NSString)
-                self?.weatherIcon = image
-            }
-        }.resume()
-    }
-}
+
 
 
 
 struct WeatherResponse: Codable {
-    let coord: Coord?
-    let weather: [Weather]?
-    let main: Main?
-    let wind: Wind?
-    let sys: Sys?
-    let name: String?
+    let coord: Coord
+    let weather: [Weather]
+    let main: Main
+    let wind: Wind
+    let sys: Sys
+    let name: String
     
     struct Coord: Codable {
         let lon: Double
