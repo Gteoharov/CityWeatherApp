380be532790eda1cc51309cd3985e251c1d86325
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-08 10:34:41 +0300

    Extend `CityWeatherURLConfig` to be able to send different Bundle instead of main

--- CityWeatherCore/CityWeatherCore/CityAPIClient/Helpers/CityWeatherURLConfig.swift
+++ CityWeatherCore/CityWeatherCore/CityAPIClient/Helpers/CityWeatherURLConfig.swift
@@ -1,17 +1,31 @@
 import Foundation
 
 public struct CityWeatherURLConfig {
+    private static var testBundle: Bundle?
+
+    private static var bundle: Bundle {
+        return testBundle ?? Bundle.main
+    }
+
     public static var searchBaseURL: URL {
-        guard let urlString = Bundle.main.infoDictionary?["SearchBaseURL"] as? String, let url = URL(string: urlString) else {
+        guard let urlString = bundle.infoDictionary?["SearchBaseURL"] as? String, let url = URL(string: urlString) else {
             fatalError("Base URL not configured correctly")
         }
         return url
     }
     
     public static var detailCityBaseURL: URL {
-        guard let urlString = Bundle.main.infoDictionary?["CityDetailBaseURL"] as? String, let url = URL(string: urlString) else {
+        guard let urlString = bundle.infoDictionary?["CityDetailBaseURL"] as? String, let url = URL(string: urlString) else {
             fatalError("Base URL not configured correctly")
         }
         return url
+    }
+    
+    public static func setTestBundle(_ bundle: Bundle) {
+        self.testBundle = bundle
+    }
+    
+    public static func resetBundle() {
+        self.testBundle = nil
     }
 }
--- CityWeatherCore/CityWeatherCoreTests/LoadCitySearchEndToEndTests.swift
+++ CityWeatherCore/CityWeatherCoreTests/LoadCitySearchEndToEndTests.swift
@@ -1,5 +1,40 @@
 import XCTest
+import CityWeatherCore
 
 final class LoadCitySearchEndToEndTests: XCTestCase {
     
+    override func setUp() {
+        super.setUp()
+        CityWeatherURLConfig.setTestBundle(Bundle(for: type(of: self)))
+    }
+    
+    override func tearDown() {
+        CityWeatherURLConfig.resetBundle()
+        super.tearDown()
+    }
+    
+    func test_endToEndTestServerGETCitySearchResult_matchesFixedTestData() async {
+        let testServerURL = CityWeatherURLConfig.searchBaseURL
+        let expectedCities = [
+            CitySearchItem(name: "Par", localNames: nil, latitude: 50.3494152, longitude: -4.7050945, country: "GB", state: "England"),
+            CitySearchItem(name: "Par", localNames: ["en" : "Par", "fa" : "پر"], latitude: 37.6809622, longitude: 45.0113965, country: "IR", state: "West Azerbaijan Province"),
+            CitySearchItem(name: "Parada", localNames: nil, latitude: 42.0222724, longitude: -8.0337175, country: "ES", state: "Galicia"),
+            CitySearchItem(name: "Parada", localNames: nil, latitude: 43.0654638, longitude: -7.6485272, country: "ES", state: "Galicia"),
+            CitySearchItem(name: "Parada", localNames: nil, latitude: 43.076012500000004, longitude: -8.450996357074423, country: "ES", state: "Galicia")
+        ]
+        
+        var urlRequest = URLRequest(url: testServerURL)
+        let client = URLSessionClient(session: URLSession(configuration: .ephemeral))
+        let loader = RemoteCitySearchLoader(request: urlRequest, client: client)
+        
+        let receivedResult: RemoteCitySearchLoader.LoadCitySearchResult = await loader.load(withQuery: "par")
+        
+        switch receivedResult {
+        case let .success(receivedCities):
+            XCTAssertFalse(receivedCities.isEmpty, "Expected sports to not be empty in the test catalogue.")
+            XCTAssertEqual(receivedCities, expectedCities)
+        case let .failure(error):
+            XCTFail("Expected successful sports result, got \(error) instead.")
+        }
+    }
 }
