1200c91450ae6019934b064d1e749fa6198f069f
Gteoharov <Gteoharov@users.noreply.github.com>
2024-08-08 09:25:32 +0300

    Moving `HTTPClientSpy` class to its own file with internal access only for test target

---
+++ CityWeatherCore/CityWeatherCoreTests/Helpers/HTTPClientSpy.swift
@@ -0,0 +1,35 @@
+import CityWeatherCore
+
+class HTTPClientSpy: HTTPClient {
+    
+    struct Stub {
+        let result: (statusCode: Int, data: Data)?
+        let error: Error?
+    }
+    
+    private(set) var sentRequest = [URLRequest]()
+    private(set) var stub: Stub?
+    
+    private let queue = DispatchQueue(label: "HTTPClientSpyQueue")
+    
+    func stub(result: (statusCode: Int, data: Data)?, error: Error?) {
+        stub = Stub(result: result, error: error)
+    }
+    
+    func perform(request: URLRequest, queryItems: [URLQueryItem]?) async -> HTTPClient.HTTPClientResult {
+        queue.sync {
+            sentRequest.append(request)
+        }
+        
+        if let error = stub?.error {
+            return .failure(error)
+        }
+        
+        if let result = stub?.result {
+            let response = HTTPURLResponse(url: request.url!, statusCode: result.statusCode, httpVersion: nil, headerFields: nil)!
+            return .success((result.data, response))
+        }
+        
+        return .failure(NSError(domain: "Empty error", code: 0))
+    }
+}
---
+++ CityWeatherCore/CityWeatherCoreTests/LoadCityDetailUseCaseTests.swift
@@ -0,0 +1,8 @@
+//
+//  LoadCityDetailUseCaseTests.swift
+//  CityWeatherCoreTests
+//
+//  Created by Georgi Teoharov on 8.08.24.
+//
+
+import Foundation
